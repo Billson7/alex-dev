e84a6e37a6900c10013066e4d8d55434a0ef1cee
Alex Billson <alex.billson@sky.uk>
2023-09-07 15:18:18 +0100

    profile picture alignment

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import profilePicture from "../Assets/profile-picture.png"
+import profilePicture from "../Assets/profile-picture.jpeg"
 
 const lorem =
   "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
