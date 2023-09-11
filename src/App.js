897510916ed8ba4d488c6c33647d2af6e089e6f8
Alex Billson <alex.billson@sky.uk>
2023-09-11 13:02:57 +0100

    small fixture structure refactor

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from "react"
 import "./App.css"
 import { Hero, Biscuit, Work, Cool } from "./components"
-import { Content } from "./fixtures/content"
+import { content } from "./fixtures/content"
 
 export const App = () => {
   // ---------- to go into a selector as it gets bigger -------
-  const { content } = Content.data
   const heroContent = content.hero
   // ----------------------------------------------------------
 
