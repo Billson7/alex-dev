4a5b6d6f1efd5a9df0224e5c0caac1699c35cc55
Alex Billson <alex.billson@sky.uk>
2023-09-11 12:59:48 +0100

    mobile hero view

--- src/components/HeroSection/hero.jsx
+++ src/components/HeroSection/hero.jsx
@@ -4,7 +4,11 @@ import profilePicture from "./Assets/profile-picture.jpeg"
 export const Hero = ({ title, caption, intro }) => (
   <section className="section section-hero">
     <div className="flex-row">
-      <img src={profilePicture} alt="dog" className="profile-image" />
+      <img
+        src={profilePicture}
+        alt="profile of Alex"
+        className="profile-image"
+      />
       <h2 className="profile-heading">{title}</h2>
       <p className="profile-caption">{caption}</p>
       <p className="profile-intro">{intro}</p>
