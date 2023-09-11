ab9712bfed5a8c98457d59d2d6c37e00e2aaa8f4
Alex Billson <alex.billson@sky.uk>
2023-09-11 12:38:35 +0100

    using content fixtures

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from "react"
 import "./App.css"
 import { Hero, Biscuit, Work, Cool } from "./components"
+import { Content } from "./fixtures/content"
 
-export const App = () => (
-  <main className="page-section">
-    <Hero />
-    <Biscuit />
-    <Work />
-    <Cool />
-  </main>
-)
+export const App = () => {
+  // ---------- to go into a selector as it gets bigger -------
+  const { content } = Content.data
+  const heroContent = content.hero
+  // ----------------------------------------------------------
+
+  return (
+    <main className="page-section">
+      <Hero {...heroContent} />
+      <Biscuit />
+      <Work />
+      <Cool />
+    </main>
+  )
+}
--- src/components/HeroSection/hero.jsx
+++ src/components/HeroSection/hero.jsx
@@ -1,17 +1,15 @@
 import React from "react"
 import profilePicture from "./Assets/profile-picture.jpeg"
 
-const lorem =
-  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
-export const Hero = () => (
+export const Hero = ({ title, caption, intro }) => (
   <section className="section section-hero">
     <div className="flex-row">
       <img src={profilePicture} alt="dog" className="profile-image" />
-      <h2 className="profile-heading">Alex Billson</h2>
-      <p className="profile-caption">://developer</p>
-      <p className="profile-intro">{lorem}</p>
+      <h2 className="profile-heading">{title}</h2>
+      <p className="profile-caption">{caption}</p>
+      <p className="profile-intro">{intro}</p>
       <code>
-        need a graphic here to push the other sections out of initial view{" "}
+        need a graphic here to push the other sections out of initial view
       </code>
     </div>
   </section>
