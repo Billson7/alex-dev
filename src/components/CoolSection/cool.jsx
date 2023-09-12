7161f9efd303b8f6fc469595b1d9035eeaad7440
Alex Billson <alex.billson@sky.uk>
2023-09-12 14:04:43 +0100

    something cool coming soon copy

--- src/components/CoolSection/cool.jsx
+++ src/components/CoolSection/cool.jsx
@@ -4,7 +4,8 @@ export const Cool = () => (
   <section className="section section-cool">
     <div className="flex-row">
       <h2 className="profile-heading">Something cool</h2>
-      <p className="profile-caption">coming soon</p>
+      <p className="profile-caption">trust me</p>
+      <p className="profile-intro">Coming soon...</p>
     </div>
   </section>
 )
