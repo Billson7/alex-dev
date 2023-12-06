0bd3713ac01702d9f627223adc542de89ad896b4
Alex Billson <alex.billson@sky.uk>
2023-12-06 13:00:22 +0000

    convert hero test file to ts

--- src/components/HeroSection/hero.spec.tsx
+++ src/components/HeroSection/hero.spec.tsx
@@ -1,32 +1,32 @@
 import React from "react"
 import { render, screen } from "@testing-library/react"
-import { Hero } from "./"
+import { Hero } from "."
 
 describe("Hero Section", () => {
 
   test("renders Hero section correctly", () => {
-    const { container } = render(<Hero />)
+    const { container } = render(<Hero title='title' caption="caption" intro='intro'/>)
     
     expect(container).toMatchSnapshot()
   })
 
 
   test("renders Hero section heading", () => {
-    render(<Hero />)
+    render(<Hero title='title' caption="caption" intro='intro'/>)
     
     expect(screen.getByTestId('profile-heading')).toBeInTheDocument();
   })
 
   test("renders Hero section caption", () => {
-    render(<Hero />)
+    render(<Hero title='title' caption="caption" intro='intro'/>)
     
     expect(screen.getByTestId('profile-caption')).toBeInTheDocument();
   })
 
   test("renders Hero section intro", () => {
-    render(<Hero />)
+    render(<Hero title='title' caption="caption" intro='intro' />)
     
-    expect(screen.getByTestId('profile-caption')).toBeInTheDocument();
+    expect(screen.getByTestId('profile-intro')).toBeInTheDocument();
   })
 
 })
