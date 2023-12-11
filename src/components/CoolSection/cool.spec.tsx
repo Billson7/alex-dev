4df8c5ac8f5148b90565b71072be3762d0cae5a7
Alex Billson <alex.billson@sky.uk>
2023-12-11 15:18:58 +0000

    convery cool test file to tsx

--- src/components/CoolSection/cool.spec.tsx
+++ src/components/CoolSection/cool.spec.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { render } from "@testing-library/react"
-import { Cool } from "./"
+import { Cool } from "."
 
 test("renders Cool section correctly", () => {
   const { container } = render(
