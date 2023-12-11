3f27e37d81758d150e72e465e816fac6ab19c481
Alex Billson <alex.billson@sky.uk>
2023-12-11 15:27:12 +0000

    fixing props that were the wrong way around

--- src/components/CoolSection/cool.tsx
+++ src/components/CoolSection/cool.tsx
@@ -3,14 +3,14 @@ import { BasicContent } from "../types"
 
 export const Cool = ({
   heading,
-  intro,
   caption,
+  intro,
 }: BasicContent): ReactElement => (
   <section className="section section-cool">
     <div className="flex-row">
       <h2 className="profile-heading">{heading}</h2>
-      <p className="profile-caption">{intro}</p>
-      <p className="profile-intro">{caption}</p>
+      <p className="profile-caption">{caption}</p>
+      <p className="profile-intro">{intro}</p>
     </div>
   </section>
 )
--- src/components/WorkSection/work.tsx
+++ src/components/WorkSection/work.tsx
@@ -1,12 +1,12 @@
 import React from "react"
 import { BasicContent } from "../types"
 
-export const Work = ({ heading, intro, caption }: BasicContent) => (
+export const Work = ({ heading, caption, intro }: BasicContent) => (
   <section className="section section-work">
     <div className="flex-row">
       <h2 className="profile-heading">{heading}</h2>
-      <p className="profile-caption-invert">{intro}</p>
-      <p className="profile-intro">{caption}</p>
+      <p className="profile-caption-invert">{caption}</p>
+      <p className="profile-intro">{intro}</p>
       <code style={{ display: "none" }}>
         github things? screenshots? links?
       </code>
