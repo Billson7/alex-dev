4b5049b444ff0ff5a5bbc8220c6826f4ba5b5322
Alex Billson <alex.billson@sky.uk>
2024-06-14 12:15:44 +0100

    adding observer logic

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,20 @@ export const App: FC = () => {
   const { heroContent, biscuitContent, workContent, coolContent } =
     getContent(content)
 
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      console.log(entry)
+      if (entry.isIntersecting) {
+        entry.target.classList.add("is-visible")
+      } else {
+        entry.target.classList.remove("is-visible")
+      }
+    })
+  })
+
+  const section = document.querySelectorAll(".is-hidden");
+  section.forEach((el) => observer.observe(el));
+
   return (
     <main className="page-section">
       <Hero {...heroContent} />
